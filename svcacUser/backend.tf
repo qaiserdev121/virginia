78cb93d8c58b29022022a25bd5b8e8616be073e0
qaiserdev121 <muhammad.masoo121@gmail.com>
2026-02-05 07:09:49 -0500

    svc

--- svcacUser/backend.tf
+++ svcacUser/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "ResourceGroup1"
     storage_account_name = "storageaccount3cat"
     container_name       = "scontainer1"
-    key                  = "multipleusers.tfstate"
+    key                  = "svcacuser.tfstate"
   }
 }
