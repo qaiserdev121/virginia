64a005162af7b404018ddbde847c28449d376821
qaiserdev121 <muhammad.masoo121@gmail.com>
2026-02-11 00:25:46 -0500

    vm

--- linuxvmmodule/main.tf
+++ linuxvmmodule/main.tf
@@ -62,9 +62,9 @@ resource "azurerm_linux_virtual_machine" "vm" {
     #storage_account_type = "Standard_LRS"
   #}
 
-  #source_image_reference {
+  source_image_reference { }
    # publisher = "Canonical"
    # offer     = "0001-com-ubuntu-server-jammy"
    # sku       = "22_04-lts"
    # version   = "latest"
-  #}
+  #
