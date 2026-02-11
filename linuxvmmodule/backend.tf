d3eaccae91cf2df768bc8859b47e3c297cb410b4
qaiserdev121 <muhammad.masoo121@gmail.com>
2026-02-11 00:52:27 -0500

    vm

--- linuxvmmodule/backend.tf
+++ linuxvmmodule/backend.tf
@@ -4,6 +4,6 @@ terraform {
     resource_group_name  = "ResourceGroup1"
     storage_account_name = "storageaccount3cat"
     container_name       = "scontainer1"
-    key                  = "oldvmlinux.tfstate"
+    key                  = "vmfresh.tfstate"
   }
 }
