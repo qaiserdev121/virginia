f4b085719e780cdc790778a0c6428505022e6ebc
qaiserdev121 <muhammad.masoo121@gmail.com>
2026-02-17 19:20:33 -0500

    vm

--- linuxvmmodule/provider.tf
+++ linuxvmmodule/provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.100"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
--- linuxvmmodule/versions.tf
+++ linuxvmmodule/versions.tf
@@ -1,10 +1,19 @@
-terraform {
-  required_version = ">= 1.5.0"
+#terraform {
+ # required_version = ">= 1.5.0"
 
+  #required_providers {
+   # azurerm = {
+    #  source  = "hashicorp/azurerm"
+     # version = ">= 3.0.0"
+    #}
+  #}
+#}
+
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = "~> 3.100"
     }
   }
 }
